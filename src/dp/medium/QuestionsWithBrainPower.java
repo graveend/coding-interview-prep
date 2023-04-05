7801b202b5a1f50e4c1b46a1422a2ef3cec80dd9
Geethu Raveendran <geethu.raveendran2110@gmail.com>
2023-04-05 12:11:56 +0100

    [DP][Medium] - Questions with Brain Power

---
+++ src/dp/medium/QuestionsWithBrainPower.java
@@ -0,0 +1,35 @@
+package dp.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QuestionsWithBrainPower {
+    public static long mostPoints(int[][] questions) {
+        int N = questions.length;
+        long[] dp = new long[N];
+        for(int i = N-1; i >= 0; i--) {
+            int futureIdx = i + questions[i][1] + 1;
+            if(futureIdx < N) {
+                dp[i] = questions[i][0] + dp[futureIdx];
+            } else {
+                dp[i] = questions[i][0];
+            }
+            if(i + 1 < N) {
+                dp[i] = Math.max(dp[i+1], dp[i]);
+            }
+        }
+        return dp[0];
+    }
+}
+
+class TestQuestionsWithBrainPower {
+
+    @Test
+    public void testBrainPower() {
+        int[][] questions = new int[][]{{5,2},{6,1},{4,4},{2,5}};
+        long expected = 8;
+        long output = QuestionsWithBrainPower.mostPoints(questions);
+        assertEquals(expected, output);
+    }
+}
